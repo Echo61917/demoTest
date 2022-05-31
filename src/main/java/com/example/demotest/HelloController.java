0da15865dc2ae8626d706df6b3f3c802de3aa492
Echo61917 <98851748+Echo61917@users.noreply.github.com>
2022-05-31 12:35:24 -0700

    asdf

--- src/main/java/com/example/demotest/HelloController.java
+++ src/main/java/com/example/demotest/HelloController.java
@@ -8,7 +8,13 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Label guess;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void onAButtonClick(){guess.setText("Good guess");}
 }
